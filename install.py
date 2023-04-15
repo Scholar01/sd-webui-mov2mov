087f2d53b878512ca8747a59a1f60f040780ad5f
Loong <69568351+Looong01@users.noreply.github.com>
2023-04-15 20:36:04 +0800

    修复Linux环境下无法合成视频的问题

--- install.py
+++ install.py
@@ -4,7 +4,10 @@ import launch
 if not launch.is_installed("cv2"):
     print('Installing requirements for Mov2mov')
     launch.run_pip("install opencv-python", "requirements for opencv")
+    launch.run_pip("install imageio", "requirements for imageio")
+
 
 if not launch.is_installed('ffmpeg'):
     print('Installing requirements for Mov2mov')
     launch.run_pip("install ffmpeg", "requirements for ffmpeg")
+    launch.run_pip("install install imageio-ffmpeg", "requirements for imageio-ffmpeg")
